8500c3e555467feef2611558d29352be70b95022
Kang Yonghyeon <109471063+kyhyeon0302@users.noreply.github.com>
2023-03-09 01:24:29 +0900

    Create 1.평균구하기.cpp

---
+++ 프로그래머스/lv1/1.평균구하기.cpp
@@ -0,0 +1,21 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12944?language=cpp
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+
+    for(int i=0; i<arr.size(); i++) {
+        answer += arr[i];
+
+
+    }
+    return answer;
+
+
+
+
+}
